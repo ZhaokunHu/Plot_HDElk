5dcbe92dc00c84e15529d147da54bb9ee3947bff
Zhaokun_Hu <aj3023211557@163.com>
2022-12-18 20:47:17 +0800

    更改了颜色和大小

---
+++ yEd_draw/Top.v
@@ -0,0 +1,48 @@
+// Generator : SpinalHDL v1.8.0b    git head : 761a30e521263983ddf14de3592f7a9f38bf0589
+// Component : Top
+// Git hash  : 268e2d739b744f1cf48925bb6d6634d262699993
+
+`timescale 1ns/1ps
+
+module Top (
+  input               io_idata_valid,
+  output              io_idata_ready,
+  input      [7:0]    io_idata_payload,
+  output              io_odata_valid,
+  input               io_odata_ready,
+  output     [7:0]    io_odata_payload
+);
+
+  wire                myBusFifo_1_io_push_ready;
+  wire                myBusFifo_1_io_pop_valid;
+  wire       [7:0]    myBusFifo_1_io_pop_payload;
+
+  MyBusFifo myBusFifo_1 (
+    .io_push_valid   (io_idata_valid                 ), //i
+    .io_push_ready   (myBusFifo_1_io_push_ready      ), //o
+    .io_push_payload (io_idata_payload[7:0]          ), //i
+    .io_pop_valid    (myBusFifo_1_io_pop_valid       ), //o
+    .io_pop_ready    (io_odata_ready                 ), //i
+    .io_pop_payload  (myBusFifo_1_io_pop_payload[7:0])  //o
+  );
+  assign io_idata_ready = myBusFifo_1_io_push_ready;
+  assign io_odata_valid = myBusFifo_1_io_pop_valid;
+  assign io_odata_payload = myBusFifo_1_io_pop_payload;
+
+endmodule
+
+module MyBusFifo (
+  input               io_push_valid,
+  output              io_push_ready,
+  input      [7:0]    io_push_payload,
+  output              io_pop_valid,
+  input               io_pop_ready,
+  output     [7:0]    io_pop_payload
+);
+
+
+  assign io_pop_valid = io_push_valid;
+  assign io_push_ready = io_pop_ready;
+  assign io_pop_payload = io_push_payload;
+
+endmodule
